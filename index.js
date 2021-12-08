33b4cb6a665aa3d5f3f9696527cdf17843c61409
bhanustark <bhanustark@gmail.com>
2021-12-09 04:22:31 +0530

    Created views and routes for login and sign up

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("Trying again...!!");
+//creating constants of required modules
+require('dotenv').config()
+const express = require('express');
+const bodyParser = require('body-parser');
+
+//created app constant for express class
+const app = express();
+
+//extended app features with other modules
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded( {extended: true }));
+app.use(express.static(__dirname + '/public'))
+
+//app routing
+app.get("/", function(req, res){
+  res.render("home");
+});
+
+app.get("/signup", function(req, res) {
+  res.render("signup");
+});
+
+//app server port setup
+app.listen(3000, function() {
+  console.log("Server started on 3000 port");
+})
